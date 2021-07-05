47a2f22e95644733566ba09c9d21b071228bb38c
Batuhan <akcay.batu76@gmail.com>
2021-07-05 15:45:14 +0300

    Rock Paper Scissors Project still in progress

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
+let computerScore = 0;
+let playerScore = 0;
+
+const scoreHuman = document.getElementById('scoreHuman');
+const scoreComputer = document.getElementById('scoreComputer');
+
 function computerPlay(){
     let computerRPS = ["Rock", "Paper", "Scissors"];
     return computerRPS[Math.floor(Math.random() * 3)];
 }
 
-function playRound(playerSelection = prompt("Pick One!"), computerSelection = computerPlay()) {
-    playerSelection.toLowerCase();
-    if ((playerSelection == "rock" && computerSelection == "Scissors") || 
-        (playerSelection == "paper" && computerSelection == "Rock") || 
-        (playerSelection == "scissors" && computerSelection == "Paper")) 
+
+const buttons = document.querySelectorAll("#buttons > button");
+buttons.forEach((button) => {
+    button.addEventListener('click', function(e){
+        let playerSelection = e.target;
+        let computerSelection = e.target;
+        playRound(playerSelection, computerSelection);
+    });       
+});
+
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+    const rock = buttons[0];
+    const paper = buttons[1];
+    const scissors = buttons[2];
+    const round = document.getElementById('round');
+    if ((playerSelection == rock && computerSelection == "Scissors") || 
+        (playerSelection == paper && computerSelection == "Rock") || 
+        (playerSelection == scissors && computerSelection == "Paper")) 
     {
-        return "You Win!";
+        round.textContent = "You Win";
     }
-    else if ((playerSelection == "rock" && computerSelection == "Paper") || 
-             (playerSelection == "paper" && computerSelection == "Scissors") || 
-             (playerSelection == "scissors" && computerSelection == "Rock"))
+    else if ((playerSelection == rock && computerSelection == "Paper") || 
+             (playerSelection == paper && computerSelection == "Scissors") || 
+             (playerSelection == scissors && computerSelection == "Rock"))
     {
-        return "You Lose!";
-    }
-    else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()){
-        return "It's a Draw";
+        round.textContent = "You Lose";
     }
     else{
-        return "Please pick Rock, Paper or Scissors.";
+        round.textContent = "Tie";
     }
+    game();
 }
 
 game = () =>{
-    for (let index = 0; index < 5; index++) {
-        console.log(playRound());       
+    if (round.textContent = "You Win") {
+        scoreHuman.textContent = playerScore + 1;
     }
+    else if (round.textContent = "You Lose") {
+        scoreComputer.textContent = computerScore + 1;
+    }    
 }
-game();
