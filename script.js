d879fe03edf7ececd8d6aa0575834da9d8bc461a
Batuhan <akcay.batu76@gmail.com>
2021-07-06 12:39:45 +0300

    Rock, Paper, Scissors still in progress

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 let computerScore = 0;
 let playerScore = 0;
 
+const round = document.getElementById('round');
 const scoreHuman = document.getElementById('scoreHuman');
 const scoreComputer = document.getElementById('scoreComputer');
 
+// Computer random pick function
 function computerPlay(){
     let computerRPS = ["Rock", "Paper", "Scissors"];
     return computerRPS[Math.floor(Math.random() * 3)];
 }
 
-
+// Onclick, players choice function
 const buttons = document.querySelectorAll("#buttons > button");
 buttons.forEach((button) => {
     button.addEventListener('click', function(e){
@@ -19,24 +21,27 @@ buttons.forEach((button) => {
     });       
 });
 
-
+// Check the computers choice and players choice
 function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay();
     const rock = buttons[0];
     const paper = buttons[1];
     const scissors = buttons[2];
-    const round = document.getElementById('round');
     if ((playerSelection == rock && computerSelection == "Scissors") || 
         (playerSelection == paper && computerSelection == "Rock") || 
         (playerSelection == scissors && computerSelection == "Paper")) 
     {
         round.textContent = "You Win";
+        playerScore++;
+        scoreHuman.textContent = "Human: " + playerScore;
     }
     else if ((playerSelection == rock && computerSelection == "Paper") || 
              (playerSelection == paper && computerSelection == "Scissors") || 
              (playerSelection == scissors && computerSelection == "Rock"))
     {
         round.textContent = "You Lose";
+        computerScore++;
+        scoreComputer.textContent = "Computer: " + computerScore;
     }
     else{
         round.textContent = "Tie";
@@ -44,11 +49,13 @@ function playRound(playerSelection, computerSelection) {
     game();
 }
 
-game = () =>{
-    if (round.textContent = "You Win") {
-        scoreHuman.textContent = playerScore + 1;
+// Check final score and stop game
+game = () => {
+    if (playerScore == 5) {
+        round.textContent = "Youuuuuu Winnnnnn";
     }
-    else if (round.textContent = "You Lose") {
-        scoreComputer.textContent = computerScore + 1;
-    }    
+    else if (computerScore == 5) {
+        round.textContent = "Youuuu Loseeeee";
+    }
+    return;
 }
